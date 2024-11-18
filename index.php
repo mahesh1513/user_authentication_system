3557ab3e601a8d6af953ae9d696a06f1b1fff0ae
mahesh1513 <68457439+mahesh1513@users.noreply.github.com>
2024-11-18 18:00:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
 
 include_once __DIR__ . '/Routes.php';
-$url = trim($_REQUEST['url']);
-$sanitized_url = filter_var($url, FILTER_SANITIZE_URL);
-$router = new Routes();
-$router->route($sanitized_url);
+if(isset($_REQUEST['url'])) {
 
+    $url = trim($_REQUEST['url']);
+    $sanitized_url = filter_var($url, FILTER_SANITIZE_URL);
+    $router = new Routes();
+    $router->route($sanitized_url); 
+
+} else {
+
+    header("Location:index.php?url=login");
+}
 
 ?>
